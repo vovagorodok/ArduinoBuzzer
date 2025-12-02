184e7f8169610f9467d7fd7bb2616ff90b8f9303
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-12-02 23:24:01 +0100

    Added template play

--- examples/sound/sound.ino
+++ examples/sound/sound.ino
@@ -1,48 +1,37 @@
 #include <ArduinoBuzzer.h>
 #include <Arduino.h>
 
-const Note SOUND_OK[] = {
+const Note SOUND_OK[] {
   { NOTE_G4, 65  },
   { NOTE_C5, 65  },
   { NOTE_E5, 65  },
   { NOTE_F5, 130 }
 };
-constexpr size_t SOUND_OK_SIZE = sizeof(SOUND_OK) / sizeof(Note);
-
-const Note SOUND_NOK[] = {
+const Note SOUND_NOK[] {
   { NOTE_F5, 65  },
   { NOTE_E5, 65  },
   { NOTE_C5, 65  },
   { NOTE_G4, 130 }
 };
-constexpr size_t SOUND_NOK_SIZE = sizeof(SOUND_NOK) / sizeof(Note);
-
-const Note SOUND_ACCEPT[] = {
+const Note SOUND_ACCEPT[] {
   { NOTE_F3,     65 },
   { NOTE_SILENT, 50 },
   { NOTE_B4,     65 }
 };
-constexpr size_t SOUND_ACCEPT_SIZE = sizeof(SOUND_ACCEPT) / sizeof(Note);
-
-const Note SOUND_REJECT[] = {
+const Note SOUND_REJECT[] {
   { NOTE_B4,     65 },
   { NOTE_SILENT, 50 },
   { NOTE_F3,     65 }
 };
-constexpr size_t SOUND_REJECT_SIZE = sizeof(SOUND_REJECT) / sizeof(Note);
-
-const Note SOUND_DROP[] = {
+const Note SOUND_DROP[] {
   { NOTE_F3, 50 },
   { NOTE_B4, 50 }
 };
-constexpr size_t SOUND_DROP_SIZE = sizeof(SOUND_DROP) / sizeof(Note);
-
-const Note SOUND_REDROP[] = {
+const Note SOUND_REDROP[] {
   { NOTE_F3, 50 },
   { NOTE_B4, 50 },
   { NOTE_F3, 50 }
 };
-constexpr size_t SOUND_REDROP_SIZE = sizeof(SOUND_REDROP) / sizeof(Note);
 
 #define BUZZER 1
 Buzzer buzzer(BUZZER);
@@ -65,22 +54,22 @@ void loop() {
     switch (count)
     {
     case 0:
-      buzzer.play(SOUND_OK, SOUND_OK_SIZE);
+      buzzer.play(SOUND_OK);
       break;
     case 1:
-      buzzer.play(SOUND_NOK, SOUND_NOK_SIZE);
+      buzzer.play(SOUND_NOK);
       break;
     case 2:
-      buzzer.play(SOUND_ACCEPT, SOUND_ACCEPT_SIZE);
+      buzzer.play(SOUND_ACCEPT);
       break;
     case 3:
-      buzzer.play(SOUND_REJECT, SOUND_REJECT_SIZE);
+      buzzer.play(SOUND_REJECT);
       break;
     case 4:
-      buzzer.play(SOUND_DROP, SOUND_DROP_SIZE);
+      buzzer.play(SOUND_DROP);
       break;
     case 5:
-      buzzer.play(SOUND_REDROP, SOUND_REDROP_SIZE);
+      buzzer.play(SOUND_REDROP);
       break;
     default:
       break;
--- src/ArduinoBuzzer.h
+++ src/ArduinoBuzzer.h
@@ -9,10 +9,16 @@ public:
 
   void begin();
   void pull();
+
   void play(const Note* notes, size_t size);
   void play(Note note);
   void stop();
   bool isPlaying() const;
+
+  template <typename T>
+  void play(const T& notes) {
+    play(notes, sizeof(notes) / sizeof(Note));
+  }
 
 private:
   const uint8_t num;
